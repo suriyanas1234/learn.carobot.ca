a7692b6b16267bf794e1a85f7c3473e966b1f0d4
Jacky <jacky@carobot.ca>
2022-02-03 15:18:32 -0500

    initial commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/jacky/projects/carobot-learn/node_modules/infima/dist/css/default/default.css'),
+  require('/home/jacky/projects/carobot-learn/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/jacky/projects/carobot-learn/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/home/jacky/projects/carobot-learn/src/css/custom.css'),
+];
